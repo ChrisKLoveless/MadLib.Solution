17dd5d429e750c8075dd56b94daa9adb64b05329
Student <student@epicodus-7.hsd1.or.comcast.net>
2022-12-12 10:11:28 -0800

    add basic project structure and boilerplate code
    co-authored-by: Robert Bryan <robbryandev@gmail.com>

---
+++ MadLib/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MadLib
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllersWithViews();
+
+			WebApplication app = builder.Build();
+
+			app.UseDeveloperExceptionPage();
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Mad}/{action=Index}/{id?}"
+			);
+
+			app.Run();
+		}
+	}
+}
